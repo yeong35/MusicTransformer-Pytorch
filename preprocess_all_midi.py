3a1fd5cabc5b653c3ba7ea66593deb6b6d3d5d71
yeong35 <e_yeong@naver.com>
2021-11-01 16:59:48 +0900

    update preprocess_all_midi.py
    midi file을 MusicTransformer 전처리에 맞게 pikle파일로 바꿔주는 파일 업데이트

---
+++ preprocess_all_midi.py
@@ -0,0 +1,76 @@
+import argparse
+import os
+import pickle
+import json
+
+import third_party.midi_processor.processor as midi_processor
+
+# prep_midi
+def prep_midi(maestro_root, output_dir):
+    """
+    ----------
+    Author: Damon Gwinn
+    ----------
+    Pre-processes the maestro dataset, putting processed midi data (train, eval, test) into the
+    given output folder
+    ----------
+    """
+
+    output_path = os.path.join(output_dir)
+    os.makedirs(output_path, exist_ok=True)
+
+    midi_list = os.listdir(maestro_root)
+
+    for piece in midi_list:
+        mid         = os.path.join(maestro_root, piece)
+        f_name      = mid.split("/")[-1] + ".pickle"
+
+        o_file = os.path.join(output_path, f_name)
+
+        prepped = midi_processor.encode_midi(mid)
+
+        o_stream = open(o_file, "wb")
+        pickle.dump(prepped, o_stream)
+        o_stream.close()
+    return True
+
+
+
+# parse_args
+def parse_args():
+    """
+    ----------
+    Author: Damon Gwinn
+    ----------
+    Parses arguments for preprocess_midi using argparse
+    ----------
+    """
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("maestro_root", type=str, help="Root folder for the Maestro dataset")
+    parser.add_argument("-output_dir", type=str, default="./dataset/e_piano", help="Output folder to put the preprocessed midi into")
+
+    return parser.parse_args()
+
+# main
+def main():
+    """
+    ----------
+    Author: Damon Gwinn
+    ----------
+    Entry point. Preprocesses maestro and saved midi to specified output folder.
+    ----------
+    """
+
+    args            = parse_args()
+    maestro_root    = args.maestro_root
+    output_dir      = args.output_dir
+
+    print("Preprocessing midi files and saving to", output_dir)
+    prep_midi(maestro_root, output_dir)
+    print("Done!")
+    print("")
+
+if __name__ == "__main__":
+    main()
